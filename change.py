93f7282f05760d0e4dc984665cdfaf4474055524
Mariano-Faraldo <mfaraldo@mail.austral.edu.ar>
2025-08-25 03:52:06 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -1,24 +1,19 @@
-def change():   
-    gasto = 23.75
-    pagado = 100
- 
-    print ("Gasto ingresado: ")
-    print (f"{gasto}")
-    print ("Dinero recibido: ")
-    print (f"{pagado}")
-
-    vuelto = pagado - gasto
-
+def change():
+    expense = 23.75
+    money = 100
+    vuelto = money - expense
     pesos = int(vuelto)
-    centavos = round((vuelto - pesos) * 100 )
+    centavos = int(round((vuelto - pesos) * 100))
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
 
-    print("vuelto\n")
+    print("\nVuelto\n")
     print("Pesos:")
     print(pesos)
-    print("centavos:")
+    print("Centavos:")
     print(centavos)
-
 change()
 
-
-# Mariano faraldo
+# Mariano Faraldo
